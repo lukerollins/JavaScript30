1b7452a07ad0a01df6b501ec600fdfc5801795fc
Luke Rollins <lukerllns@gmail.com>
2019-03-23 20:11:32 -0400

    Thought some quick changes were in order

--- 12 - Key Sequence Detection/js/magicSummoning.js
+++ 12 - Key Sequence Detection/js/magicSummoning.js
@@ -1,11 +1,11 @@
 const pressed = [];
-  const secretCode = 'keysersoze'
+  const secretCode = 'unicorn'
   window.addEventListener('keyup', (e) => {
     console.log(e.key);
     pressed.push(e.key);
     pressed.splice(-secretCode.length -1, pressed.length - secretCode.length);
     if(pressed.join('').includes(secretCode)) {
-      console.log('Verbal Kynt is Keyser Soze!!!');
+      console.log('I SUMMON A UNICORN!!!');
       cornify_add();
     }
     console.log(pressed);
